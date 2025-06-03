7e0bdc60eabc1f50abff3761d0f1a47bf0305681
Mishach <mishachkld@gmail.com>
2025-06-03 18:25:44 +0200

    add Blazor project

---
+++ Castles.WebAPI/Controllers/CastleController.cs
@@ -0,0 +1,39 @@
+﻿using Castles.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Castles.WebAPI.Controllers;
+
+public class CastleController : Controller
+{
+    [HttpGet]
+    public IActionResult Castles()
+    {
+        return View();
+    }
+    
+    [HttpGet]
+    public IActionResult Castle(Guid id)
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult CastleCreate(Castle castleNew)
+    {
+        return Ok();
+    }
+    
+    [HttpDelete]
+    public IActionResult CastleDelete(Guid id)
+    {
+        return View();
+    }
+
+    [HttpPut]
+    public IActionResult CastleUpdate(Castle updatedCastle)
+    {
+        return Ok();
+    }
+    
+    
+}
